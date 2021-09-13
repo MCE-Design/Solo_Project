121a4e03b34f805e612debc20a4ff2a9a41ba269
Michael E <mceng.design@gmail.com>
2021-09-13 14:37:59 -0700

    Added Schema and Test Seeder

---
+++ backend/db/migrations/20210913173445-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { unique } = require("faker");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userName: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+        unique: true,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(256),
+        unique: true,
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
