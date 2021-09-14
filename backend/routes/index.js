428ffda45ccd36195454c51c321ba6dcf058bab8
Michael E <mceng.design@gmail.com>
2021-09-13 23:12:29 -0700

    Finished backend (basic)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,11 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res){
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World');
-});
+// Left in just in case further debugging is needed
+
+// router.get('/hello/world', function(req, res){
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World');
+// });
 
 module.exports = router;
